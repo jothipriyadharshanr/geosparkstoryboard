81ca3d4b2694d98847ef4e0075b4ea0ac250302f
jothipriyadharshanr <joe@geospark.co>
2021-04-22 09:44:21 +0530

    Initial Commit

---
+++ geosparkexample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  geosparkexample
+//
+//  Created by GeoSpark Mac #3 on 22/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
